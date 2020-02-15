70aca6e7844575333afe844122f371223171e1a3
matszc <matiz305@hotmail.com>
2020-02-15 11:24:28 +0100

    firebase files

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://szedarserverapi20200205035200.azurewebsites.net/api',
+    //baseURL: 'https://szedarserverapi20200205035200.azurewebsites.net/api',
+    baseURL: 'https://localhost:5001/api',
     headers: {
         'Content-Type': 'application/json',
     },
